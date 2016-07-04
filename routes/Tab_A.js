99151deb86d5f10b8f5009b3e29f3a9761c836c6
gimun <gimunlee@kaist.ac.kr>
2016-07-04 22:25:57 +0900

    changing prefix

--- routes/Tab_A.js
+++ routes/Tab_A.js
@@ -1,24 +1,24 @@
 var Contacs = require('../models/contacs.js');
+var prefix = '/A';
 module.exports = function(app)
 {
     // GET ALL BOOKS
-    app.get('/A/api/books', function(req,res){
+    app.get(prefix+'/api/books', function(req,res){
         Contacs.find(function(err, books){
             if(err) return res.status(500).send({error: 'database failure'});
             res.json(books);
         })
     });
-    app.get('/A/hello', function(req,res){
+    app.get(prefix+'/api/hello', function(req,res){
         res.writeHead(200,{"Content-Type":"text/plain"});
         console.log("hello");
         res.write("Hello A");
         res.end();
     });
-    app.get('/A/bye', function(req,res) {
+    app.get(prefix+'/api/bye', function(req,res) {
         res.writeHead(200,{"Content-Type":"text/plain"});
         console.log("bye");
         res.write("Bye A");
         res.end();
     });
-         
 }
