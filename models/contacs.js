d8a4189b4ac2f293a6f8420e2f21fd62bcde3a00
gimun <gimunlee@kaist.ac.kr>
2016-07-04 21:59:47 +0900

    chnage names of schema

--- models/contacs.js
+++ models/contacs.js
@@ -1,10 +1,12 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var bookSchema = new Schema({
-    title: String,
-    author: String,
-    published_date: { type: Date, default: Date.now  }
+var contactSchema = new Schema({
+    name: String,
+    email: String,
+    phone: String
+    // phone: { type: Date, default: Date.now  }
 });
 
-module.exports = mongoose.model('book', bookSchema);
+// module.exports = mongoose.model('book', bookSchema);
+module.exports = mongoose.model('contact', contactSchema);
--- models/ground.js
+++ models/ground.js
@@ -1,10 +1,10 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var bookSchema = new Schema({
+var book2Schema = new Schema({
     title: String,
     author: String,
     published_date: { type: Date, default: Date.now  }
 });
 
-module.exports = mongoose.model('book', bookSchema);
+module.exports = mongoose.model('book2', bookSchema);
