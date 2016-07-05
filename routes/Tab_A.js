737e83c9d6bf7f51874b55993e55c405c4764802
gimun <gimunlee@kaist.ac.kr>
2016-07-05 19:31:55 +0900

    Finish of single user contacts

--- routes/Tab_A.js
+++ routes/Tab_A.js
@@ -2,13 +2,6 @@ var contacts = require('../models/contacts.js');
 var PREFIX = '/A';
 
 module.exports = function (app, db) {
-    // GET ALL BOOKS
-    app.get(PREFIX + '/api/books', function (req, res) {
-        Contacs.find(function (err, books) {
-            if (err) return res.status(500).send({ error: 'database failure' });
-            res.json(books);
-        })
-    });
     app.get(PREFIX + '/api/hello', function (req, res) {
         res.writeHead(200, { "Content-Type": "text/plain" });
         console.log("hello");
