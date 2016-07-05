eb57952e9e53efc6d04939dff923117a1b97ea52
gimun <gimunlee@kaist.ac.kr>
2016-07-05 14:30:57 +0900

    dropping before uploading

--- upload.js
+++ upload.js
@@ -7,11 +7,14 @@ var db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function() {
 	console.log("MongoDB Connected");
+	db.collections['contacts'].drop(function(err) {
+		console.log('collection dropped');
+	})
 });
 
 var gimun = new contacts({name: "Gimun",
-													email: "gimunlee@kaist.ac.kr",
-													phone: "010-8866-3321"});
+						email: "gimunlee@kaist.ac.kr",
+						phone: "010-8866-3321"});
 console.log("Gimun's name is : " + gimun.name);
 gimun.save(function (err, gimun) {
 	if(err)
