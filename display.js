b6552b7a4f7928d8710c92690d3c8808c4889027
gimun <gimunlee@kaist.ac.kr>
2016-07-05 15:28:47 +0900

    remove null exception catch

--- display.js
+++ display.js
@@ -10,8 +10,8 @@ db.once("open", function () {
     console.log("MongoDB Connected");
 });
 
-contacts.find(function(err, contactsList) {
-    if(err)
+contacts.find(function (err, contactsList) {
+    if (err)
         return console.error(err);
     console.log(contactsList);
     db.close();
--- routes/Tab_A.js
+++ routes/Tab_A.js
@@ -1,43 +1,34 @@
 var contacts = require('../models/contacts.js');
 var PREFIX = '/A';
 
-module.exports = function(app, db)
-{
+module.exports = function (app, db) {
     // GET ALL BOOKS
-    app.get(PREFIX+'/api/books', function(req,res){
-        Contacs.find(function(err, books){
-            if(err) return res.status(500).send({error: 'database failure'});
+    app.get(PREFIX + '/api/books', function (req, res) {
+        Contacs.find(function (err, books) {
+            if (err) return res.status(500).send({ error: 'database failure' });
             res.json(books);
         })
     });
-    app.get(PREFIX+'/api/hello', function(req,res){
-        res.writeHead(200,{"Content-Type":"text/plain"});
+    app.get(PREFIX + '/api/hello', function (req, res) {
+        res.writeHead(200, { "Content-Type": "text/plain" });
         console.log("hello");
         res.write("Hello A");
         res.end();
     });
-    app.get(PREFIX+'/api/bye', function(req,res) {
-        res.writeHead(200,{"Content-Type":"text/plain"});
+    app.get(PREFIX + '/api/bye', function (req, res) {
+        res.writeHead(200, { "Content-Type": "text/plain" });
         console.log("bye");
         res.write("Bye A");
         res.end();
     });
-    app.get(PREFIX+'/contacts', function(req,res) {
-        // console.log("Sending sample data of size 2");
-        contacts.find(function(err, contactsList) {
-            if(err)
+    app.get(PREFIX + '/contacts', function (req, res) {
+        contacts.find(function (err, contactsList) {
+            if (err)
                 return console.error(err);
-            if(contactsList) {
-                console.log("Sending list : " + contactsList);
-                res.write(contactsList.toString());
-                res.end();
-            }
-            else {
-                res.write("empty");
-                res.end();
-            }
+
+            console.log("Sending list : " + contactsList);
+            res.write(contactsList.toString());
+            res.end();
         });
-        // res.write("[{\"name\":\"Gimun\", \"email\":\"gimunlee@kaist.ac.kr\", \"phone\":\"010-8866-3321\"},{\"name\":\"Dongmin\", \"email\":\"dongmin.seo@kaist.ac.kr\", \"phone\":\"010-seo-3321\"}]");
-        // res.end();
     })
 }
