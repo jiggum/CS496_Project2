ecbdff0a8707b15b964b590d8f19624f0cec70c6
gimun <gimunlee@kaist.ac.kr>
2016-07-05 14:37:20 +0900

    saving with callback

--- upload.js
+++ upload.js
@@ -5,20 +5,23 @@ mongoose.connect("mongodb://localhost/test");
 
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function() {
+db.once("open", function () {
 	console.log("MongoDB Connected");
-	db.collections['contacts'].drop(function(err) {
+	db.collections['contacts'].drop(function (err) {
 		console.log('collection dropped');
+		console.log("Gimun's name is : " + gimun.name);
+		gimun.save(function (err, gimun) {
+			if (err)
+				return console.error(err);
+			db.close();
+		});
 	})
 });
 
-var gimun = new contacts({name: "Gimun",
-						email: "gimunlee@kaist.ac.kr",
-						phone: "010-8866-3321"});
-console.log("Gimun's name is : " + gimun.name);
-gimun.save(function (err, gimun) {
-	if(err)
-		return console.error(err);
-	db.close();
+var gimun = new contacts({
+	name: "Gimun",
+	email: "gimunlee@kaist.ac.kr",
+	phone: "010-8866-3321"
 });
+
 
