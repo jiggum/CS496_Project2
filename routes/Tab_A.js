635b46e36a4eb03e0887205de7ade32a5c379877
gimun <gimunlee@kaist.ac.kr>
2016-07-05 20:38:48 +0900

    not found error

--- routes/Tab_A.js
+++ routes/Tab_A.js
@@ -5,7 +5,7 @@ var PREFIX = '/A';
 module.exports = function (app) {
     app.get(PREFIX + '/api/hello', function (req, res) {
         var fid = req.query.fid;
-        var helloStr ="Hello, " + fid + ". This is A" 
+        var helloStr = "Hello, " + fid + ". This is A"
         res.writeHead(200, { "Content-Type": "text/plain" });
         console.log(helloStr);
         res.write(helloStr);
@@ -19,34 +19,37 @@ module.exports = function (app) {
         res.write(byeStr);
         res.end();
     });
-    app.post(PREFIX + '/api/signup', function(req,res) {
+    app.post(PREFIX + '/api/signup', function (req, res) {
 
     })
     app.get(PREFIX + '/contacts', function (req, res) {
-        console.log(User.count({fid:req.query.fid}).toString());
-        if(User.count({fid:req.query.fid})==0) {
+        console.log(User.count({ fid: req.query.fid }).toString());
+        if (User.count({ fid: req.query.fid }) == 0) {
             console.log(fid + " not found.");
             res.write(id + " not found.");
             res.end();
-            return;
         }
-        var json = User.findOne({fid:req.query.fid},function (err, user) {
-            res.write(user.contacts);
-            res.end();
-        })
+        else {
+            var json = User.findOne({ fid: req.query.fid }, function (err, user) {
+                res.write(user.contacts);
+                res.end();
+            });
+        }
     });
     app.post(PREFIX + '/contacts', function (req, res) {
-        console.log(User.count({fid:req.query.fid}).toString());
-        if(User.count({fid:req.query.fid})==0) {
+        console.log(User.count({ fid: req.query.fid }).toString());
+        if (User.count({ fid: req.query.fid }) == 0) {
             console.log(fid + " not found.");
             res.write(fid + " not found.");
             return;
         }
-        User.findOneAndUpdate({fid:req.query.fid},
-                            {$set: {contacts:req.body.toString()}});
-        res.writeHead(201);
-        console.log(req.body.toString());
-        res.write("Updated with posted json");
-        res.end()
+        else {
+            User.findOneAndUpdate({ fid: req.query.fid },
+                { $set: { contacts: req.body.toString() } });
+            res.writeHead(201);
+            console.log(req.body.toString());
+            res.write("Updated with posted json");
+            res.end();
+        }
     });
 }
