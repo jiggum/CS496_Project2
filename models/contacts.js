cb88c893ff156f4e218d3288237c1723f25e9e37
gimun <gimunlee@kaist.ac.kr>
2016-07-05 23:52:04 +0900

    adding type

--- models/contacts.js
+++ models/contacts.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var contactSchema = new Schema({
+    type: String,
     name: String,
     email: String,
     phone: String
--- routes/Tab_A.js
+++ routes/Tab_A.js
@@ -23,7 +23,7 @@ module.exports = function (app) {
     app.get(PREFIX + '/contacts', function (req, res) {
 
         contactModel = mongoose.model(req.query.fid + "contact", contactSchema, req.query.fid);
-        contactModel.find(function (err, contactsList) {
+        contactModel.find({type:"contact"},function (err, contactsList) {
             if (err) return console.error(err);
             var json = "[" + contactsList + "]";
             res.write(json);
@@ -32,9 +32,10 @@ module.exports = function (app) {
     });
     app.post(PREFIX + '/contacts', function (req, res) {
         contactModel = mongoose.model(req.query.fid + "contact", contactSchema, req.query.fid);
-        contactModel.remove({}, function (err) {
+        contactModel.remove({type:"contact"}, function (err) {
             for (var i = 0; i < req.body.length; i++) {
                 var contact = new contactModel({
+                    type: "contact",
                     name: req.body[i].name,
                     email: req.body[i].email,
                     phone: req.body[i].phone
