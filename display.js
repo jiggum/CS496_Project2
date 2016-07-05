cbf5f3c5e27a53818fb8f2107ed1c741bf3ef708
gimun <gimunlee@kaist.ac.kr>
2016-07-05 14:43:18 +0900

    display

---
+++ display.js
@@ -0,0 +1,17 @@
+var mongoose = require("mongoose");
+var contacts = require("./models/contacts.js");
+
+mongoose.connect("mongodb://localhost/test");
+
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+
+db.once("open", function () {
+    console.log("MongoDB Connected");
+});
+
+contacts.find(function(err, contactsList) {
+    if(err)
+        return console.error(err);
+    console.log(contactsList);
+});
