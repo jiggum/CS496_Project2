526f3e801637d456cc4b6d148a7cd7a702d125d9
gimun <gimunlee@kaist.ac.kr>
2016-07-05 20:28:34 +0900

    editing for user

--- routes/Tab_A.js
+++ routes/Tab_A.js
@@ -1,49 +1,34 @@
 var User = require('../models/user.js');
-var Contacts = require('../models/contacts.js');
+var Contact = require('../models/contacts.js');
 var PREFIX = '/A';
 module.exports = function (app) {
     app.get(PREFIX + '/api/hello', function (req, res) {
-        var id = req.query.id;
-        var helloStr ="Hello, " + id + ". This is A" 
+        var fd = req.query.fid;
+        var helloStr ="Hello, " + fid + ". This is A" 
         res.writeHead(200, { "Content-Type": "text/plain" });
         console.log(helloStr);
         res.write(helloStr);
         res.end();
     });
     app.get(PREFIX + '/api/bye', function (req, res) {
-        var id = req.query.id;
-        var byeStr = "Bye, " + id + ". Sincerely A";
+        var fid = req.query.fid;
+        var byeStr = "Bye, " + fid + ". Sincerely A";
         res.writeHead(200, { "Content-Type": "text/plain" });
         console.log(byeStr);
         res.write(byeStr);
         res.end();
     });
     app.get(PREFIX + '/contacts', function (req, res) {
-        var json = user.contacts;
-
-        res.write(json);
-        res.end();
+        var json = User.findOne({fid:req.query.fid},function (err, user) {
+            res.write(user.contacts);
+            res.end();
+        })
     });
     app.post(PREFIX + '/contacts', function (req, res) {
-        db.collections['contacts'].drop(function (err) {
-            console.log('collection dropped');
-            for (var i = 0; i < req.body.length; i++) {
-                console.log("Person " + i + req.body[i].name);
-                // console.log(req.body[i].name);
-                // console.log(req.body[i].email);
-                // console.log(req.body[i].phone);
-                var contact = new contacts({
-                    name: req.body[i].name,
-                    email: req.body[i].email,
-                    phone: req.body[i].phone
-                });
-                contact.save(function (err, contact) {
-                    if (err)
-                        return console.error(err);
-                });
-            }
-        });
+        User.findOneAndUpdate({fid:req.query.fid},
+                            {$set: {contacts:req.body.toString()}});
         res.writeHead(201);
+        console.log(req.body.toString());
         res.write("Updated with posted json");
         res.end()
     });
