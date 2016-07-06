b1e6d8d08619569d6b0b617a8c375029153edf6b
gimun <gimunlee@kaist.ac.kr>
2016-07-07 02:15:34 +0900

    C server

--- routes/Tab_C.js
+++ routes/Tab_C.js
@@ -1,87 +1,213 @@
 var Ground = require('../models/ground.js');
-var PREFIX = '/C';
-module.exports = function(app)
-{
-    // GET ALL BOOKS
-    app.get(PREFIX+'/api/books', function(req,res){
-        Ground.find(function(err, books){
-            if(err) return res.status(500).send({error: 'database failure'});
-            res.json(books);
+var PREFIX = '/C/ground';
+var _ = require('underscore');
+var async = require('async');
+
+module.exports = function (app) {
+    //Players
+    var maxPlayers = 2;
+    var currentPlayers = 0;
+    var players = [];
+    var winnerIndex = 1;
+
+    //ports
+    var ports = []; //each player port
+    var nextPort = 10501;
+    var problemPort = 10400;
+
+    var mapSize = 25;
+    var map = _.range(mapSize).map(function (x, i) { return '-1'; });
+
+    var ress = [];
+
+    //Problem and Result || Map
+    {
+        function createProblem(len) {
+            var problem = [];
+            for (var i = 0; i < len; i++) {
+                problem.push(Math.floor(Math.random() * 100));
+            }
+            return problem;
+        }
+        function calcAnswer(problem) {
+            var sum = 0;
+            async.forEachOf(problem, function (value, key, callback) {
+                sum += value;
+            }, function (err) {
+                if (err) {
+                    console.error(err.message);
+                    sum = -1;
+                }
+                else {
+                    console.log('===== ' + problem + 's answer is ' + sum);
+                }
+            });
+            return sum;
+        }
+        var problems = [];
+        var answers = [];
+        var problemsCell = [];
+        app.get(PREFIX + '/problem', function (req, res) {
+            var playerName = req.query.fid;
+            var playerIndex = players.indexOf(playerName);
+
+            var cell = req.query.problem;
+            var problem = createProblem(5);
+            problems.unshift(problem);
+            answers.unshift(calcAnswer(problem));
+            var problemIndex = problems.indexOf(problem);
+            problemsCell.unshift(cell);
+
+            res.writeHead(200);
+            res.write(problem.toString() + ' ans : ' + answers[problemIndex]);
+            res.end();
+        });
+        app.get(PREFIX + '/result', function (req, res) {
+            var playerIndex = players.indexOf(req.query.fid);
+            var cell = req.query.problem;
+            var problemIndex = problemsCell.indexOf(cell);
+            // var problem=problems[problemIndex];
+            var answer = answers[problemIndex];
+
+            res.writeHead(200);
+            res.write('{\"result\":');
+            console.log('real answer : ' + answer + '\trequested : ' + req.query.answer);
+            if (answer == req.query.answer) {
+                map[cell] = playerIndex;
+                res.write(' \"Y\", \"map\":\"{[' + map.toString() + ']}\"}');
+
+
+
+                var flushingBuffer = mapBuffer.slice();
+                var flushingCounts = mapPlayers;
+                mapBuffer = [];
+                mapPlayers = 0;
+                for (var i = 0; i < flushingCounts; i++) {
+                    flushingBuffer[i].writeHead(200);
+                    flushingBuffer[i].write('{\"map\":[' + map.toString() + ']}');
+                    flushingBuffer[i].end();
+                }
+
+                console.log('===== flushed');
+            }
+            else
+                res.write('\"N\"}');
+            res.end();
+        });
+
+        var upToDate = _.range(maxPlayers).map(function (x, i) { return true; });
+        function resetUpToDate() { upToDate = _.range(maxPlayers).map(function (x, i) { return true; }); }
+        var mapBuffer = [];
+        var mapPlayers = 0;
+        // var mapBufferFlushing = false;
+        app.get(PREFIX + '/map', function (req, res) {
+            if (req.query.intermediate == '1') {
+                res.writeHead(200);
+                res.write('{\"map\":[' + map.toString() + ']}');
+                res.end();
+            }
+            else {
+                mapBuffer.push(res);
+                mapPlayers++;
+                /////////////////////???????????????????????//////////////
+            }
         })
-    });
+    }
 
-    // GET SINGLE BOOK
-    app.get(PREFIX+'/api/books/:book_id', function(req, res){
-        Ground.findOne({_id: req.params.book_id}, function(err, book){
-            if(err) return res.status(500).json({error: err});
-            if(!book) return res.status(404).json({error: 'book not found'});
-            res.json(book);
-        })
-    });
+    //Retire
+    {
 
-    // GET BOOK BY AUTHOR
-    app.get(PREFIX+'/api/books/author/:author', function(req, res){
-        Ground.find({author: req.params.author}, {_id: 0, title: 1, published_date: 1},  function(err, books){
-            if(err) return res.status(500).json({error: err});
-            if(books.length === 0) return res.status(404).json({error: 'book not found'});
-            res.json(books);
-        })
-    });
+        var retiredBuffer = [];
+        var retiredPlayers = 0;
+        app.get(PREFIX + '/retire', function (req, res) {
+            var playerName = req.query.fid;
+            var playerIndex = players.indexOf(playerName);
 
-    // CREATE BOOK
-    app.post(PREFIX+'/api/books', function(req, res){
-        var book = new Ground();
-        book.title = req.body.title;
-        book.author = req.body.author;
-        book.published_date = new Date(req.body.published_date);
+            retiredBuffer.push(res);
+            retiredPlayers++;
 
-        book.save(function(err){
-            if(err){
-                console.error(err);
-                res.json({result: 0});
+            if (retiredPlayers == currentPlayers) {
+                for (var i = 0; i < currentPlayers; i++) {
+                    retiredBuffer[i].writeHead(200);
+                    retiredBuffer[i].write('Winner is ' + players[winnerIndex]);
+                    retiredBuffer[i].end();
+                }
+                retiredBuffer = [];
+                retiredPlayers = 0;
+
+
+                currentPlayers = 0;
+                map = _.range(mapSize).map(function (x, i) { return '-1'; });
+                console.log('======================= GAME FINISHED ================');
+            }
+        });
+    }
+
+    //Access and Start
+    {
+
+        var readyBuffer = [];
+        var readyBufferIndex = [];
+        var readyPlayers = 0;
+        app.get(PREFIX + '/start', function (req, res) {
+            var playerName = req.query.fid;
+            var playerIndex = players.indexOf(playerName);
+
+            readyBuffer.push(res);
+            readyBufferIndex.push(playerIndex);
+            readyPlayers++;
+
+            console.log('===== user ' + playerName + ' accessed for his port');
+
+            if (readyPlayers == maxPlayers) {
+                for (var i = 0; i < readyPlayers; i++) {
+                    readyBuffer[i].writeHead(200);
+                    // readyBuffer[i].write(map.toString());
+                    readyBuffer[i].write('{\"refreshport\":\"' + ports[readyBufferIndex[i]] + '\", \"' +
+                        'problemport\":\"' + problemPort + '\"}');
+                    readyBuffer[i].end();
+                }
+                readyBuffer = [];
+                readyPlayers = 0;
+
+                app.listen(problemPort, function () {
+                    console.log('===================GAME START =================');
+                })
+            }
+        });
+        app.get(PREFIX + '/access', function (req, res) {
+            var playerName = req.query.fid;
+            if (players.indexOf(playerName) != -1) {
+                var index = players.indexOf(playerName);
+                res.writeHead(200);
+                res.write('Access with your port : ' + ports[index]);
+                res.end();
                 return;
             }
+            if (currentPlayers == maxPlayers) {
+                res.writeHead(404);
+                res.write('Server is full');
+                res.end();
+            }
+            else {
+                var playerIndex = currentPlayers;
+                currentPlayers++;
+                var newPort = nextPort;
+                nextPort++;
 
-            res.json({result: 1});
+                players.push(playerName);
+                ports.push(newPort);
 
+                app.listen(newPort, function () {
+                    console.log('===== Listening for user # ' + playerIndex + ' ' + playerName + ' on port ' + newPort + '...');
+                });
+
+                console.log('===== user # ' + currentPlayers + ' ' + playerName + ' accessed. port : ' + newPort);
+                res.writeHead(200);
+                res.write((newPort).toString());
+                res.end();
+            }
         });
-    });
+    }
 
-    // UPDATE THE BOOK
-    app.put(PREFIX+'/api/books/:book_id', function(req, res){
-        Ground.update({ _id: req.params.book_id }, { $set: req.body }, function(err, output){
-            if(err) res.status(500).json({ error: 'database failure' });
-            console.log(output);
-            if(!output.n) return res.status(404).json({ error: 'book not found' });
-            res.json( { message: 'book updated' } );
-        })
-    /* [ ANOTHER WAY TO UPDATE THE BOOK ]
-            Ground.findById(req.params.book_id, function(err, book){
-            if(err) return res.status(500).json({ error: 'database failure' });
-            if(!book) return res.status(404).json({ error: 'book not found' });
-            if(req.body.title) book.title = req.body.title;
-            if(req.body.author) book.author = req.body.author;
-            if(req.body.published_date) book.published_date = req.body.published_date;
-            book.save(function(err){
-                if(err) res.status(500).json({error: 'failed to update'});
-                res.json({message: 'book updated'});
-            });
-        });
-    */
-    });
-
-    // DELETE BOOK
-    app.delete(PREFIX+'/api/books/:book_id', function(req, res){
-        Ground.remove({ _id: req.params.book_id }, function(err, output){
-            if(err) return res.status(500).json({ error: "database failure" });
-
-            /* ( SINCE DELETE OPERATION IS IDEMPOTENT, NO NEED TO SPECIFY )
-            if(!output.result.n) return res.status(404).json({ error: "book not found" });
-            res.json({ message: "book deleted" });
-            */
-
-            res.status(204).end();
-        })
-    });
-     
 }
